a7b9cefde04c59a376d35e7d37addb68d5e60d62
nersus15 <fathur.ashter15@gmail.com>
2020-11-28 10:07:06 +0800

    menambah parameter di function fieldmapping

--- application/helpers/formloader_helper.php
+++ application/helpers/formloader_helper.php
@@ -2,18 +2,32 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 if(!method_exists($this,'field')){
-    function fieldmapping($config, $input){
+    function fieldmapping($config, $input, $defaultValue = array(), $petaNilai = array()){
 
         /** @var CI_Controller $ci */
         $ci =& get_instance();
         $isLoaded = $ci->load->config('forms');
         $configitem = $ci->config->item('form');
+        $adaDefault = count($defaultValue) > 0;
+        $adaPeta = count($petaNilai) > 0;
         $field = array();
         if(!$isLoaded || empty($configitem[$config]))
             response(['message' => 'Config form ' . $config . ' Tidak ditemukan'], 404);
+        
         foreach($configitem[$config] as $k => $v){
             if(isset($input[$k]))
                 $field[$v] = html_escape($input[$k]);
+            elseif(!isset($input[$k]) && $adaDefault && isset($defaultValue[$k]))
+                $field[$v] = $defaultValue[$k];
+        }
+
+        if($adaPeta){
+            foreach($petaNilai as $f){
+                foreach($f as $k => $v){
+                    if($field[$f] == $k)
+                        $field[$f] = $v;
+                }
+            }
         }
         return $field;
     }
